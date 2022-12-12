81f527cf37dadeaa6275e56a2c4998d81613c7a0
Katerin Tello Ura <u.kate.tello@gmail.com>
2022-12-11 20:16:34 -0500

    add ui material refresher

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, ButtonGroup, styled, Typography } from "@mui/material";
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function App() {
+
+
+  const BlueButton = styled(Button)(({theme})=>({
+    backgroundColor:theme.palette.otherColor.main,
+    color:"#888",
+    margin:5,
+    "&:hover":{
+      backgroundColor:"violet"
+    },
+    "&:disabled":{
+      backgroundColor:"gray",
+      color:"white"
+    }
+  }));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Button variant="text" startIcon={<SettingsIcon />}>Text</Button>
+      <Button variant="contained" color="secondary">Contained</Button>
+      <Button variant="outlined" disabled>Outlined</Button>
+      <Typography variant="h1" component="p">
+        h1. Heading
+      </Typography>
+      <Button variant="contained" color="otherColor">
+        My Unique Button
+      </Button>
+      <BlueButton>
+        One button
+      </BlueButton>
+      <BlueButton>
+        Other button
+      </BlueButton>
+
     </div>
   );
 }
